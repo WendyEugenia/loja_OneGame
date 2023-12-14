1fea4363c996bfa3eb12c032a5814d2eb43ed228
WendyEugenia <wendy.t.n@hotmail.com>
2023-12-14 12:13:41 -0300

    criando projeto loja oneGame

---
+++ src/main/java/com/generation/lojaonegame/LojaOneGameApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojaonegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOneGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOneGameApplication.class, args);
+	}
+
+}
